922d36f1dd67b5fd0bdbb1fee4ee463477d7a1a0
Lardzzz <aku.ikan04@gmail.com>
2023-04-12 14:26:51 +0700

    Add project files.

---
+++ OOP1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+private :
+	int nim;
+	string nama;
+public :
+	void inputdata() {
+		cout << "Masukkan NIM : ";
+		cin >> nim;
+		cout << "Masukkan nama : ";
+		cin >> nama
+	}
+	void tampil
+};
