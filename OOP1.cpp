62e4593e790fe02255cbdc3b17717f20381556da
Lardzzz <aku.ikan04@gmail.com>
2023-04-12 14:33:37 +0700

    Memberikan akses publik ke method inputdata dan tampildata

--- OOP1.cpp
+++ OOP1.cpp
@@ -5,3 +5,15 @@ class Mahasiswa {
 private:
 	int nim;
 	string nama;
+public:
+	void inputdata() {
+		cout << "Masukkan NIM : ";
+		cin >> nim;
+		cout << "Masukkan nama : ";
+		cin >> nama
+	}
+	void tampildata() {
+		cout << "\nNIM : " << nim << endl;
+		cout << "Nama : " << nama << endl;
+	}
+};
