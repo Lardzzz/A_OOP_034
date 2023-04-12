33ac38ffed1a74674fa57325053594b7b071bb7c
Lardzzz <aku.ikan04@gmail.com>
2023-04-12 14:27:46 +0700

    deklarasi kelas mahasiswa

--- OOP1.cpp
+++ OOP1.cpp
@@ -2,15 +2,3 @@
 using namespace std;
 
 class Mahasiswa {
-private :
-	int nim;
-	string nama;
-public :
-	void inputdata() {
-		cout << "Masukkan NIM : ";
-		cin >> nim;
-		cout << "Masukkan nama : ";
-		cin >> nama
-	}
-	void tampil
-};
